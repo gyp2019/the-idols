e7b6148c2aaa5e635869a2782911f96517efea90
bomber <youngphil.gim@gmail.com>
2019-03-30 23:15:54 +0900

    add header, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IdolsComponent } from './dashboard/idols/idols.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IdolsComponent
+    IdolsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
